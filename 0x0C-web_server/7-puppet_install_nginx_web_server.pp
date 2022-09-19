852b25c965a3820e3d09a6af97a7f3efa824fadd
Dewinsy <mushdaisy71@gmail.com>
2022-09-19 12:55:41 +0300

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,6 +2,6 @@
 # install nginx with puppet
 
 exec { 'nginx':
-command  => 'sudo apt-get update && sudo apt-get install -y nginx && echo "Holberton School for the win!" | sudo tee /var/www/html/index.html && sed -i "/server_name _;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default && sudo service nginx restart',
+command  => 'sudo apt-get update && sudo apt-get install -y nginx && echo "Hello World!" | sudo tee /var/www/html/index.html && sed -i "/server_name _;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default && sudo service nginx restart',
 provider => shell,
 }
